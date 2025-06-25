f1b9de1571672975815b1713fc51258905389551
bvbej <admin@bvbej.com>
2025-06-25 15:36:38 +0800

    [fix] 代码格式修复

--- src/adb/adbprocessimpl.h
+++ src/adb/adbprocessimpl.h
@@ -8,27 +8,27 @@ class AdbProcessImpl : public QProcess
     Q_OBJECT
 
 public:
-    explicit AdbProcessImpl(QObject *parent = nullptr);
+    explicit AdbProcessImpl(QObject* parent = nullptr);
     virtual ~AdbProcessImpl();
 
-    void execute(const QString &serial, const QStringList &args);
-    void forward(const QString &serial, quint16 localPort, const QString &deviceSocketName);
-    void forwardRemove(const QString &serial, quint16 localPort);
-    void reverse(const QString &serial, const QString &deviceSocketName, quint16 localPort);
-    void reverseRemove(const QString &serial, const QString &deviceSocketName);
-    void push(const QString &serial, const QString &local, const QString &remote);
+    void execute(const QString& serial, const QStringList& args);
+    void forward(const QString& serial, quint16 localPort, const QString& deviceSocketName);
+    void forwardRemove(const QString& serial, quint16 localPort);
+    void reverse(const QString& serial, const QString& deviceSocketName, quint16 localPort);
+    void reverseRemove(const QString& serial, const QString& deviceSocketName);
+    void push(const QString& serial, const QString& local, const QString& remote);
     void pull(const QString& serial, const QString& remote, const QString& local);
-    void install(const QString &serial, const QString &local);
-    void removePath(const QString &serial, const QString &path);
+    void install(const QString& serial, const QString& local);
+    void removePath(const QString& serial, const QString& path);
     bool isRuning();
-    void setShowTouchesEnabled(const QString &serial, bool enabled);
+    void setShowTouchesEnabled(const QString& serial, bool enabled);
     QStringList getDevicesSerialFromStdOut();
     QString getDeviceIPFromStdOut();
     QString getDeviceIPByIpFromStdOut();
     QString getStdOut();
     QString getErrorOut();
 
-    static const QString &getAdbPath();
+    static const QString& getAdbPath();
 
 signals:
     void adbProcessImplResult(qsc::AdbProcess::ADB_EXEC_RESULT processResult);
